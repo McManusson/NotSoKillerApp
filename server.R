f5289c294f3f910bb63ac5cbe33a7ee59fd9a02d
unknown <pesfan@gmail.com>
2018-10-06 19:17:07 +0200

    Actualización de server.R

--- server.R
+++ server.R
@@ -6,4 +6,20 @@ function(input, output) {
   library(tesseract)
   library(stringr)
   library(dplyr)
+  observeEvent(input$upload, {
+    if(input$option == "scan") {
+      output_s = ocr_data(file)
+    } else {
+      output_c = pdf_text(file)
+    }
+  })
+  observeEvent(input$preview, {
+    if(exists(output_s)) {
+      output$preview = verbatimTextOutput(output[1:10,])
+    } else if(exists(output_c)) {
+      output$preview = verbatimTextOutput(output[1])
+    } else {
+      output$preview = renderUI(HTML("<strong>No preview available</strong>"))
+    }
+  })
 }
