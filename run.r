47e7a5eb263bdbaf55102597b14d1caa3b3911b7
RaVeN1987 <tomas.castano.baena@gmail.com>
2018-10-07 13:12:28 +0200

    cambios en la home sin maquetar

---
+++ run.r
@@ -0,0 +1,8 @@
+require(shiny)
+folder_address = 'C:/Users/tomas/OneDrive/Documents/NotSoKillerApp'
+runApp(folder_address, launch.browser=TRUE)
+getwd()
+.libPaths('C:/[path]')
+library(methods)
+shiny::runApp("shinyapp/", launch.browser=TRUE)'
+
--- ui.R
+++ ui.R
@@ -3,18 +3,44 @@ ui <- fluidPage(
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
   ),
-  tags$header("Upload app"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file", "Choose file",
-                multiple = FALSE,
-                accept = c("text/pdf",
-                           "text/comma-separated-values,text/plain",
-                           ".pdf", ".jpg", ".png")),
-      radioButtons("option", NULL,
-                  c("Scan", "Computer")),
-      actionButton("preview", "Preview!")
-    ),
-    mainPanel(uiOutput("preview"))
-  )
+    tabsetPanel(
+      tabPanel("",
+               # 
+               #   column(
+               #     12, align = 'center', 
+               #     h1("Personal Hunters"), 
+               #     img( src = "Banco_Santander_Logotipo.png"), 
+               #     h4('Actinio Application'),h2('Welcome', style = 'color: black;'), 
+               #     actionButton('welcome','Login', style = 'color:white; background:#FE0000; border:#FE0000', width = 120,  align = 'center' ))
+               # )
+               fluidRow(column(12,
+               includeHTML("./www/index.html")
+                  )
+                )
+               ),
+      tabPanel("", verbatimTextOutput("summary"),
+               sidebarLayout(
+                 sidebarPanel(
+                   fileInput("file", "Choose file",
+                             multiple = FALSE,
+                             accept = c("text/pdf",
+                                        "text/comma-separated-values,text/plain",
+                                        ".pdf", ".jpg", ".png")),
+                   radioButtons("option", NULL,
+                                c("Scan", "Computer")),
+                   actionButton("preview", "Preview!")
+                 ),
+                 mainPanel(uiOutput("preview"))
+               )
+               
+               ),
+      tabPanel("", tableOutput("table")
+               
+               
+               
+               )
+    )
+
+
 )
+
